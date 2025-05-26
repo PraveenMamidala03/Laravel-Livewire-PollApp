ac556de613209bf8bd106141ac505955cc9a5378
Praveen <praveenmamidala1095@gmail.com>
2025-05-26 13:20:35 +0530

    Polls fetched

---
+++ app/Http/Livewire/Polls.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Polls extends Component
+{
+    protected $listeners = ['pollCreated' => 'render'];
+    public function render()
+    {
+        $polls = \App\Models\Poll::with('options.votes')->latest()->get();
+
+        return view('livewire.polls', ['polls' => $polls]);
+    }
+}
