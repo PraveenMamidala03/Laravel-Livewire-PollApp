e984151ea403b6e02342984b284abf81d6456ae9
Praveen <praveenmamidala1095@gmail.com>
2025-05-26 15:08:21 +0530

    final update

--- app/Http/Livewire/Polls.php
+++ app/Http/Livewire/Polls.php
@@ -2,15 +2,25 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Option;
 use Livewire\Component;
 
 class Polls extends Component
 {
-    protected $listeners = ['pollCreated' => 'render'];
+    protected $listeners = [
+        'pollCreated' => 'render'
+    ];
+
     public function render()
     {
-        $polls = \App\Models\Poll::with('options.votes')->latest()->get();
+        $polls = \App\Models\Poll::with('options.votes')
+            ->latest()->get();
 
         return view('livewire.polls', ['polls' => $polls]);
+    }
+
+    public function vote(Option $option)
+    {
+        $option->votes()->create();
     }
 }
